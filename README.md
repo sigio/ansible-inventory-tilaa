87661193a30e6f851be26e3cf2e652b7e94cc522
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2018-09-18 13:00:00 +0200

    Tilaa-with-replace version, with info in README

--- README.md
+++ README.md
@@ -26,3 +26,19 @@ From the Tilaa api documentation at https://www.tilaa.com/en/api/docs:
   cannot use your main Tilaa user account to access this API. Instead,
   you should add an API user to your customer account and use the
   specified login credentials to authenticate.
+
+# Comment
+
+Tilaa doesn't allow any dots or underscores in their 'friendly' hostnames
+that are presented in the webinterface and API. Since we prefer to use the
+fqdn-hostname here, we like to use dots in our hostnames.
+
+The tilaa-with-replace.py version, also included in the repository will
+find/replace double dashes (--) with a dot in the hostname, so if you
+creatively name your hosts, this find/replace will return fqdn-hostnames.
+
+demo.sig-io.nl would become demo--sig-io--nl in the tilaa api/control-panel,
+but back to demo.sig-io.nl for Ansible/AWX/Tower.
+
+I have requested tilaa to allow dots or underscores, but have not received
+a reply yet.
---
+++ tilaa-with-replace.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# coding: utf-8
+
+# Copyright (c) 2018 Cybersprint
+#
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the software. If not, see <http://www.gnu.org/licenses/>.
+
+'''a dynamic Ansible inventory source based on the Tilaa API'''
+
+import argparse
+import requests
+import json
+import os
+import sys
+
+user = os.environ['TILAA_USERNAME']
+password = os.environ['TILAA_PASSWORD']
+
+
+url = 'https://api.tilaa.com/v1/virtual_machines'
+
+req = requests.get(url, auth=(user, password))
+
+content = req.content
+content = json.loads(content)
+output = {}
+names = []
+networks = {}
+
+for entry in content['virtual_machines']:
+    # Find/replace '--' in hostname to '.', so we can have the actual hostname
+    newname = entry['name'].replace("--", ".")
+    names.append(newname)
+
+    networks[newname] = {
+            'ansible_host': entry['network'][0]['address'],
+            'tilaa_vars': entry
+        }
+
+output['tilaa'] = names
+output['_meta'] = {'hostvars': {}}
+
+for n in names:
+    output['_meta']['hostvars'] = networks
+
+
+print(json.dumps(output))
+
